f7d2e6329da0156f5b174aa92e70fe0c2452cf72
Vitor Gabriel <66925616+vgabriel10@users.noreply.github.com>
2024-02-08 15:17:03 -0300

    Adicionado Repository

---
+++ Repositories/Interfaces/IProdutoRepository.cs
@@ -0,0 +1,10 @@
+﻿using API_Avaliacao_Produtos_Servicos.Data;
+
+namespace API_Avaliacao_Produtos_Servicos.Repositories.Interfaces
+{
+    public interface IProdutoRepository
+    {
+        public void FindAll();
+        public void FindById(int Id);
+    }
+}
---
+++ Repositories/ProdutoRepository.cs
@@ -0,0 +1,25 @@
+﻿using API_Avaliacao_Produtos_Servicos.Data;
+using API_Avaliacao_Produtos_Servicos.Repositories.Interfaces;
+
+namespace API_Avaliacao_Produtos_Servicos.Repositories
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutoRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FindById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
