a9f6c1695463b2392e6e23544654db7ebacf55e0
vgabriel10 <vittorgabriell420@gmail.com>
2024-06-03 22:26:47 -0300

    .

--- Validators/ProdutoValidator.cs
+++ Validators/ProdutoValidator.cs
@@ -8,8 +8,21 @@ namespace API_Avaliacao_Produtos_Servicos.Validators
     {
         public ProdutoValidator() 
         {
+            RuleFor(x => x.Nome)
+                .NotEmpty().WithMessage("Informe o nome do produto")
+                .Length(3, 100).WithMessage("O produto deve ter entre 3 e 100 caracteres");
+
+            RuleFor(x => x.Preco)
+                .NotEmpty().WithMessage("O produto deve ter um preço")
+                .GreaterThan(0).WithMessage("O preço deve ser maior que zero");
+
             
 
+            //RuleFor(x => x.FornecedorId)
+            //    .NotEmpty().WithMessage("Informe o id do fornecedor do produto")
+            //    .Must(dado => Numero(dado).WithMessage("O Id do fornecedor deve ser um número")
+
         }
+
     }
 }
