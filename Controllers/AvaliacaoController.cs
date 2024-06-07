a72eab46bf82dcbdb4e4793b6a73d1aeb455529a
vgabriel10 <vittorgabriell420@gmail.com>
2024-06-07 20:49:26 -0300

    criando endpoints

--- Controllers/AvaliacaoController.cs
+++ Controllers/AvaliacaoController.cs
@@ -1,5 +1,6 @@
 ﻿using API_Avaliacao_Produtos_Servicos.Data;
 using API_Avaliacao_Produtos_Servicos.Models;
+using API_Avaliacao_Produtos_Servicos.Models.InputModels;
 using API_Avaliacao_Produtos_Servicos.Repositories.Interfaces;
 using API_Avaliacao_Produtos_Servicos.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
@@ -20,7 +21,45 @@ namespace API_Avaliacao_Produtos_Servicos.Controllers
         [HttpGet("avaliacao")]
         public IActionResult Get()
         {
-            return Ok();
+            var todasAvaliacoes = _avaliacaoService.RetornaTodasAvaliacoes();
+            if (todasAvaliacoes != null)
+                return Ok(todasAvaliacoes);
+
+            return BadRequest();
+        }
+
+        [HttpGet("avaliacao/{idProduto}")]
+        public IActionResult Get(int idProduto)
+        {
+            var avaliacoesDoProduto = _avaliacaoService.RetornaAvaliacoesDoProduto(idProduto);
+            if (avaliacoesDoProduto != null)
+                return Ok(avaliacoesDoProduto);
+
+            return NotFound();
+        }
+
+        [HttpPost("avaliacao")]
+        public IActionResult Post(AvaliacaoInputModel avaliacao)
+        {
+            var result = _avaliacaoService.AdicionarAvaliacao(avaliacao);
+            if (result != null)
+                return Ok(result);
+
+            return BadRequest();
+        }
+
+        [HttpPut("avaliacao/{idAvaliacao}")]
+        public IActionResult Put([FromRoute]int idAvaliacao, [FromBody] AvaliacaoInputModel avaliacao)
+        {
+            //var result = _avaliacaoService.EditarAvaliacao()
+            return BadRequest();
+        }
+
+        [HttpDelete("avaliacao/{idAvaliacao}")]
+        public IActionResult Delete([FromRoute] int idAvaliacao)
+        {
+            //_avaliacaoService.RemoverAvaliacao(idAvaliacao)
+            return BadRequest();
         }
 
     }
