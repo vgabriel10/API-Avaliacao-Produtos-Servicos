67318a7e1542d3c232abfb820bc8beee925f2d5e
vgabriel10 <vittorgabriell420@gmail.com>
2024-08-08 22:40:11 -0300

    iniciando configuracao de um midware que captura as excessoes e padroniza as respostas

---
+++ Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using API_Avaliacao_Produtos_Servicos.Models.ViewModels;
+using Newtonsoft.Json;
+
+namespace API_Avaliacao_Produtos_Servicos.Middlewares
+
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An unexpected error occurred.");
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var response = new ExceptionViewModel(false, "Ocorreu um erro", new List<string> { exception.Message });
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            return context.Response.WriteAsync(JsonConvert.SerializeObject(response));
+        }
+    }
+}
---
+++ Models/ViewModels/ExceptionViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace API_Avaliacao_Produtos_Servicos.Models.ViewModels
+{
+    public class ExceptionViewModel
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public List<string> Errors { get; set; }
+
+        public ExceptionViewModel(bool success = true, string message = null, List<string> errors = null)
+        {
+            Success = success;
+            Message = message;
+            Errors = errors ?? new List<string>();
+        }
+    }
+}
